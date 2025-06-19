32ed2288d761e34ca54f8b396ba30bd8c998e632
Malik Johnson <60333731+MalikJohnson@users.noreply.github.com>
2025-06-19 03:53:58 -0400

    Initial commit

---
+++ src/Solution.java
@@ -0,0 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode current = head;
+        while(current!=null && current.next !=null){
+            if(current.val == current.next.val){
+                //skip duplicate
+                current.next = current.next.next;
+            }
+            //move pointer up
+            current = current.next;
+        }
+        return head;
+    }
+}
